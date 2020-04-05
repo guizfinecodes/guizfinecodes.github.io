ab1e2f6b8b315f0fee034310243d483f22ba8d4c
BRIAN GUIZ <brianguis123@gmail.com>
2020-04-05 17:13:06 +0300

    edit

--- wp-content/themes/salient/js/preloader.js
+++ wp-content/themes/salient/js/preloader.js
@@ -3,26 +3,14 @@
 
 //guiz preloader
 
-var loader;
 
-function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 50);
-    }
-}
 
-function displayContent() {
-	preloaderFadeOutTime = 900;
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
+jQuery(document).ready(function($) {
 
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
-});
+        // 01. START PRELOADER
+        $(window).load(function() {
+            // Animate loader off screen
+            $(".loader").fadeOut("slow");
+        });
+        // 01. END PRELOADER
+        });
